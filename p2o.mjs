0432587273b1f303eb34decf017e2618e7664b0c
Fabio Pereira dos Santos <fabio.santos@looplex.com.br>
2023-05-04 14:57:21 -0300

    fix: adicionando Ocp-Apim-Subscription-Key e informações de contato na API

--- p2o.mjs
+++ p2o.mjs
@@ -5,7 +5,15 @@ const postman = './postman/graph.looplex.com.postman_collection.json'
 const openapi = './openapi/openapi.yaml'
 
 try {
-  const result = await postmanToOpenApi(postman, openapi, options)
+  const result = await postmanToOpenApi(postman, openapi, {
+    ...options,
+    replaceVars: true,
+    additionalVars: {
+      'Ocp-Apim-Subscription-Key': 'Ocp-Apim-Subscription-Key',
+      'Ocp-Apim-Subscription-Key-Text': '(Required) Subscription key provided by Looplex',
+      'Profile': 'Profile',
+    }
+  })
 } catch (error) {
   console.error(error)
 }
