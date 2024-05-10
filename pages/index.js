f15cc348f92e0c083193549f7786fa5dc34c9314
Fabio Pereira dos Santos <fabio.santos@looplex.com.br>
2024-05-10 20:02:36 -0300

    docs: updating project structure

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,3 @@
-import { Button, Col, Row } from 'antd'
 import Image from 'next/image'
 import { useRouter } from 'next/router'
 
@@ -7,45 +6,49 @@ export default function Home() {
   const router = useRouter()
 
   return (
-    <Row>
 
-      <Col span={4}>
-        <Image
-          src={'/logo.png'}
-          width={250}
-          height={120}
-          alt='teste'
-        />
-      </Col>
+    <>
+      <Image
+        src={'/logo.png'}
+        width={250}
+        height={120}
+        alt='teste'
+      />
 
-      <Row style={{ backgroundColor: 'green' }}>
-        <Col span={24}>
-          Bem vindo à página de documentações da Looplex
-        </Col>
-      </Row>
+      Bem vindo à página de documentações da Looplex
+      
+      <br />
 
-        <Button onClick={() => router.push('/v1/actions')}>
-          Actions v1
-        </Button>
+      <button onClick={() => router.push('/v1/actions')}>
+        Actions v1
+      </button>
 
-        <Button onClick={() => router.push('/v1/assembler')}>
-          Assembler v1
-        </Button>
+      <br />
 
-        <Button onClick={() => router.push('/v1/looplex365')}>
-          Looplex 365 v1
-        </Button>
+      <button onClick={() => router.push('/v1/assembler')}>
+        Assembler v1
+      </button>
 
-        <Button onClick={() => router.push('/v1/cases')}>
-          Cases v1
-        </Button>
+      <br />
 
-        <Button onClick={() => router.push('/v2/varas_foros')}>
-          Varas e Foros v2
-        </Button>
+      <button onClick={() => router.push('/v1/looplex365')}>
+        Looplex 365 v1
+      </button>
 
-    </Row>
-  );
+      <br />
+
+      <button onClick={() => router.push('/v1/cases')}>
+        Cases v1
+      </button>
+
+      <br />
+
+      <button onClick={() => router.push('/v2/varas_foros')}>
+        Varas e Foros v2
+      </button>
+
+    </>
+  )
 
 }
 
