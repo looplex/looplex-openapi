13a1fece3b539ec30ce8301aa0997eb8bb6e3d13
Fabio Pereira dos Santos <fabio.santos@looplex.com.br>
2024-05-13 17:23:33 -0300

    fix: updating docs
    AB#25543

--- app/page.js
+++ app/page.js
@@ -1,10 +1,82 @@
 'use client'
 
-import { Row, Col, List, Typography } from "antd"
+import { Row, Col, Typography, Tabs, Divider, Tag } from "antd"
 import Image from 'next/image'
 
 const { Text } = Typography
 
+const apis = [
+  {
+    key: 'actions',
+    label: 'Actions',
+    children:
+      <>
+        <Text style={{ fontWeight: 'bold', fontSize: 16 }}>
+          Endpoints do Actions
+        </Text>
+        <Divider />
+        <a href="/v1/actions" target="_blank">
+          Actions v1
+        </a>
+      </>
+  },
+  {
+    key: 'assembler',
+    label: 'Assembler',
+    children:
+      <>
+        <Text style={{ fontWeight: 'bold', fontSize: 16 }}>
+          Endpoints do Assembler
+        </Text>
+        <Divider />
+        <a href="/v1/assembler" target="_blank">
+          Assembler v1
+        </a>
+      </>
+  },
+  {
+    key: 'cases',
+    label: 'Cases',
+    children:
+      <>
+        <Text style={{ fontWeight: 'bold', fontSize: 16 }}>
+          Endpoints do Looplex Cases
+        </Text>
+        <Divider />
+        <a href="/v1/cases" target="_blank">
+          Cases v1
+        </a>
+      </>
+  },
+  {
+    key: 'looplex365',
+    label: 'Looplex 365',
+    children:
+      <>
+        <Text style={{ fontWeight: 'bold', fontSize: 16 }}>
+          Endpoints do Looplex 365
+        </Text>
+        <Divider />
+        <a href="/v1/looplex365" target="_blank">
+          Looplex 365 v1
+        </a>
+      </>
+  },
+  {
+    key: 'varas_foros',
+    label: 'Varas e Foros',
+    children:
+      <>
+        <Text style={{ fontWeight: 'bold', fontSize: 16 }}>
+          Endpoints de Varas e Foros
+        </Text>
+        <Divider />
+        <a href="/v2/varas_foros" target="_blank">
+          Varas e Foros v2
+        </a>
+      </>
+  }
+]
 export default function Home() {
 
   return (
@@ -17,43 +89,24 @@ export default function Home() {
             height={120}
             alt='Looplex'
           />
+
+        </Col>
+        <Col span={18} style={{ marginTop: 30 }}>
+          <Text style={{ fontWeight: 'bold', fontSize: 18 }}>
+            Bem vindo à página de documentações das APIs da Looplex
+          </Text>
         </Col>
 
-        <Col span={20}>
-
-          <List
-            bordered
-            style={{ width: '75%', marginTop: 30 }}
-            header={
-              <b style={{ fontSize: 18 }}>
-                Bem vindo à página de documentações das APIs da Looplex
-              </b>
-            }
-            dataSource={[
-              'Actions v1|/v1/actions',
-              'Assembler v1|/v1/assembler',
-              'Cases v1|/v1/cases',
-              'Looplex 365 v1|/v1/looplex365',
-              'Varas e Foros v2|/v2/varas_foros'
-            ]}
-            renderItem={(item) => {
-              let arrItem = item.split('|')
-
+        <Col span={18} offset={4}>
+          <Tabs
+            tabBarExtraContent={() => {
               return (
-                <List.Item>
-                  <a href={arrItem[1]} target="_blank">
-                    <Text>{arrItem[0]}</Text>
-                  </a>
-                </List.Item>
+                <Text>extra</Text>
               )
             }}
-            footer={
-              <b style={{ fontSize: 18 }}>
-                Looplex - 2024
-              </b>
-            }
+            tabPosition="left"
+            items={apis}
           />
-
         </Col>
 
       </Row>
